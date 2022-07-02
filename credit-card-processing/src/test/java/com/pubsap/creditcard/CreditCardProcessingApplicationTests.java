ac403b392727f044873a3226d38f2230a5ea69b1
vidhangithub <vidhan.amc@gmail.com>
2022-07-02 13:10:02 +0100

    Skelleton Code

---
+++ credit-card-processing/src/test/java/com/pubsap/creditcard/CreditCardProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubsap.creditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
